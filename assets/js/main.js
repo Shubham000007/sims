c4a0ee58e973507021cbca584fb72cf5743afb6b
Shubham rawat <rshubham589@gmail.com>
2021-09-13 23:13:27 +0530

    version 1.0.3

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// getDownDownMenu
+// const dropDownMenu = document.querySelector(".drop__down__menu");
+// if (dropDownMenu) {
+//   dropDownMenu.addEventListener("mouseover", (e) => {
+//     document.querySelector(".dropdown__container").classList.remove("d__none");
+//   });
+//   dropDownMenu.addEventListener("mouseleave", (e) => {
+//     document.querySelector(".dropdown__container").classList.add("d__none");
+//   });
+// }
